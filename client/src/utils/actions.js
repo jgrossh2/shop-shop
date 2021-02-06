1366069233389d3a67d580d64927d0e7c0333b47
Mona Lisa <joannagrosshans2@gmail.com>
2021-02-06 14:32:36 -0600

    global objects refactored

---
+++ client/src/utils/actions.js
@@ -0,0 +1,7 @@
+// want to store products data retrieved for products in global state, can add offline functionality
+export const UPDATE_PRODUCTS = "UPDATE_PRODUCTS";
+// take list of categories retrieved and store on global state, can add offline functionality
+export const UPDATE_CATEGORIES = "UPDATE_CATEGORIES";
+// connector between above, want to select category from state, and display products 
+export const UPDATE_CURRENT_CATEGORY = "UPDATE_CURRENT_CATEGORY";
+
